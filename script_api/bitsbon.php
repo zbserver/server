daacc7fdbaab958f502ae1f7c8be65393329c670
zbserver <novandrawahyuherwanto@gmail.com>
2025-01-27 06:34:12 +0700

    Update bitsbon.php

--- script_api/bitsbon.php
+++ script_api/bitsbon.php
@@ -12,6 +12,7 @@ ban();
 Function h(){
     $h[] = "Host: ".host[1];
     $h[] = "x-requested-with: XMLHttpRequest";
+    $h[] = "accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7";
     $h[] = "cookie: ".file_get_contents(Data.cok);
     $h[] = "user-agent: ".file_get_contents(Data.uag);
     return $h;
