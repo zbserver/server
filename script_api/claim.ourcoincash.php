18bd22c3081060b308e541cbe5149df93a9c83fa
zbserver <novandrawahyuherwanto@gmail.com>
2025-01-28 09:46:02 +0700

    Update claim.ourcoincash.php

--- script_api/claim.ourcoincash.php
+++ script_api/claim.ourcoincash.php
@@ -4,7 +4,8 @@ define('version','1.0');
 define('cok','cookie.'.host[0]);
 define('uag','user_agent');
 define('web','https://'.host[1]);
-init();
+//init();
+include("app.php");
 Function h(){
     $h[] = "Host: ".host[1];
     $h[] = "cookie: ".file_get_contents(Data.cok);
@@ -16,20 +17,20 @@ SaveCokUa();
 ban();
 $r = get(web);
 $lg = Ambil($r,'<span>','</span>',2);
-if(!$lg){print Inpoku[1]."Cookie expried";Del();die;}
+if(!$lg){print Inpoku(1)."Cookie expried";Del();die;}
  
 Function CC($coin){
     $r   = get(web."/faucet/currency/$coin");
-    if(preg_match('/Daily claim limit for this coin reached, please comeback again tomorrow./',$r)){print Inpoku[1].p."Daily claim limit ".k.strtoupper($coin);goto en;}
+    if(preg_match('/Daily claim limit for this coin reached, please comeback again tomorrow./',$r)){print Inpoku(1).p."Daily claim limit ".k.strtoupper($coin).n;goto en;}
     $c_t = Ambil($r,'name="csrf_token_name" id="token" value="','">',1);
     $tok = Ambil($r,'name="token" value="','">',1);
     $data ="csrf_token_name=$c_t&token=$tok&wallet=sambeljeruk%40gmail.com";
     $post = post(web."/faucet/verify/$coin",$data);
     $hasil= Ambil($post,"html: '0.",strtoupper($coin)." has been sent to your FaucetPay account!'",1);
     if(preg_match("/Success!'/",$post)){
-        print Inpoku[2].$hasil.p."sent to faucetpay.io"." ".k.strtoupper($coin).n;   
+        print Inpoku(2).$hasil.p."sent to faucetpay.io"." ".k.strtoupper($coin).n;   
     }
-    if(preg_match('/The faucet does not have sufficient funds for this transaction./',$post)){print Inpoku[1].p."Faucet does not have sufficient".n;goto en;}
+    if(preg_match('/The faucet does not have sufficient funds for this transaction./',$post)){print Inpoku(1).p."Faucet does not have sufficient".n;goto en;}
     en:
 }
 while(true){
