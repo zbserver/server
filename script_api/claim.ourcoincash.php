e8e83d5b6810736762d8889d480e11e3efeb5d18
zbserver <novandrawahyuherwanto@gmail.com>
2025-01-30 15:00:05 +0700

    Update claim.ourcoincash.php

--- script_api/claim.ourcoincash.php
+++ script_api/claim.ourcoincash.php
@@ -20,33 +20,23 @@ cl();
 ban();
 $r = get(web);
 $lg = Ambil($r,'<span>','</span>',2);
-if(!$lg){print Pesan(4,1)."Cookie expried";Del();die;}
- 
-Function CC($coin){
-    
-    $r   = get(web."/faucet/currency/$coin");
-    $c_t = Ambil($r,'name="csrf_token_name" id="token" value="','">',1);
-    $tok = Ambil($r,'name="token" value="','">',1);
-    $data ="csrf_token_name=$c_t&token=$tok&wallet=$Wallet";
-    $post = post(web."/faucet/verify/$coin",$data);
-    $hasil= Ambil($post,"html: '0.",strtoupper($coin)." has been sent to your FaucetPay account!'",1);
-    if(preg_match("/Success!'/",$post)){
-        print Inpoku(2).$hasil.p."sent to faucetpay.io"." ".k.strtoupper($coin).n;   
-    }
-    if(preg_match('/The faucet does not have sufficient funds for this transaction./',$post)){print Inpoku(1).p."Faucet does not have sufficient".n;goto en;}
-    en:
-}
+if(!$lg){print Pesan(4,1)."  Cookie expried";Del();die;}
+
 while(true){
     $r = get(web);
     $c = explode('/faucet/currency/',$r);
     foreach($c as $a => $coins){
         if($a == 0)continue;
         $coin = explode('"',$coins)[0];
+        if(preg_match("/firewall/",$r)){
+            print Pesan(4,1).p."  Firewall! Open browser".n;
+        }
+
         $r   = get(web."/faucet/currency/$coin");
         if($res){if($res[$coin] > 2)continue;}
         if(preg_match('/Daily claim limit/',$r)){
             $res = Riwayat([$coin=>3],$res);
-            print pesan(4,1).p."Daily claim limit ".w2.strtoupper($coin).p.n;continue;
+            print pesan(4,1).p."  Daily claim limit ".w2.strtoupper($coin).p.n;continue;
         }
         $c_t = Ambil($r,'name="csrf_token_name" id="token" value="','">',1);
         $tok = Ambil($r,'name="token" value="','">',1);
@@ -58,7 +48,7 @@ while(true){
         }
         if(preg_match("/Sufficient fund/",$post)){
             $res = Riwayat([$coin=>3],$res);
-            print pesan(4,1).p."Sufficient funds ".w2.strtoupper($coin).p.n;continue;
+            print pesan(4,1).p."  Sufficient funds ".w2.strtoupper($coin).p.n;continue;
             $res = Riwayat([$coin=>1],$res);
         }
         en:
