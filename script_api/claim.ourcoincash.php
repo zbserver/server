47f492743f3d429cbabc0f88da222df58e935f79
zbserver <novandrawahyuherwanto@gmail.com>
2025-01-30 14:47:25 +0700

    Update claim.ourcoincash.php

--- script_api/claim.ourcoincash.php
+++ script_api/claim.ourcoincash.php
@@ -4,7 +4,7 @@ define('version','1.0');
 define('cok','cookie.'.host[0]);
 define('uag','user_agent');
 define('web','https://'.host[1]);
-init();
+Init();
 Function h(){
     $h[] = "Host: ".host[1];
     $h[] = "cookie: ".file_get_contents(Data.cok);
@@ -14,6 +14,9 @@ Function h(){
 ban();
 SaveCokUa();
 ban();
+save("Email");
+$Wallet=file_get_contents(Data."Email");
+cl();
 $r = get(web);
 $lg = Ambil($r,'<span>','</span>',2);
 if(!$lg){print Inpoku(1)."Cookie expried";Del();die;}
@@ -21,10 +24,9 @@ if(!$lg){print Inpoku(1)."Cookie expried";Del();die;}
 Function CC($coin){
     
     $r   = get(web."/faucet/currency/$coin");
-    
     $c_t = Ambil($r,'name="csrf_token_name" id="token" value="','">',1);
     $tok = Ambil($r,'name="token" value="','">',1);
-    $data ="csrf_token_name=$c_t&token=$tok&wallet=sambeljeruk%40gmail.com";
+    $data ="csrf_token_name=$c_t&token=$tok&wallet=$Wallet";
     $post = post(web."/faucet/verify/$coin",$data);
     $hasil= Ambil($post,"html: '0.",strtoupper($coin)." has been sent to your FaucetPay account!'",1);
     if(preg_match("/Success!'/",$post)){
@@ -43,23 +45,22 @@ while(true){
         if($res){if($res[$coin] > 2)continue;}
         if(preg_match('/Daily claim limit/',$r)){
             $res = Riwayat([$coin=>3],$res);
-            print m." Daily claim limit ".p."[".w3.strtoupper($coin).p."]".n;continue;
+            print pesan(4,1).p."Daily claim limit ".w2.strtoupper($coin).p.n;continue;
         }
         $c_t = Ambil($r,'name="csrf_token_name" id="token" value="','">',1);
         $tok = Ambil($r,'name="token" value="','">',1);
         $data ="csrf_token_name=$c_t&token=$tok&wallet=sambeljeruk%40gmail.com";
         $post = post(web."/faucet/verify/$coin",$data);
-        $hasil= Ambil($post,"html: '0.",strtoupper($coin)." has been sent to your FaucetPay account!'",1);
+        $hasil= Ambil($post,"html: '",strtoupper($coin)." has been sent to your FaucetPay account!'",1);
         if(preg_match("/Success!'/",$post)){
             print pesan(4,2).pesan(5,$hasil).p."sent to faucetpay.io"." ".k.strtoupper($coin).n;   
         }
-        if(preg_match('/sufficient funds/',$post)){print Inpoku(1).p."Faucet does not have sufficient".n;goto en;}
         if(preg_match("/Sufficient fund/",$post)){
             $res = Riwayat([$coin=>3],$res);
-            print p." Sufficient funds ".p."[".w3.strtoupper($coin).p."]".n;continue;
+            print pesan(4,1).p."Sufficient funds ".w2.strtoupper($coin).p.n;continue;
             $res = Riwayat([$coin=>1],$res);
         }
         en:
+        //if(Riwayat($res) > 2)break;
     }
-    if(min($res) > 2)break;
 }
