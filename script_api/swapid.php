b6d357d6e87ff67825522ad340c2fc7df7e2519d
zbserver <novandrawahyuherwanto@gmail.com>
2025-02-13 01:33:13 +0700

    Update swapid.php

--- script_api/swapid.php
+++ script_api/swapid.php
@@ -5,25 +5,7 @@ define('cok','cookie.'.host[0]);
 define('uag','user_agent');
 define('web','https://'.host[1]);
 init();
-apikey:
 ban();
-/*Print " ".Pesan(0, "Menu apikey").n;
-Menu(1,"Xevil");
-Menu(2,"Multibot");
-$pilih = readline(" ".Pesan(0,"Input ".p).panah.p);
-if($pilih == 1){
-    $api_url="http://api.sctg.xyz";
-    Print w3." Xevil : ".p.n;
-    Save("Apikey");
-}elseif($pilih == 2){
-    $api_url="http://api.multibot.in";
-    Print w3." Multibot : ".p.n;
-    Save("Apikey");  
-}else{print k." Bad Number".n;sleep(3);goto apikey;}
-
-if(!file_exists(Data."Apikey")){
-    goto apikey;
-}*/
 Awal:
 SaveCokUa();
 ban();
@@ -48,43 +30,17 @@ Function Game($game,$id){
         $data ="score=5004&csrf=$csrf";
         $r = post(web."/games/verify?id=$id",$data);
         $oke = Ambil($r,'earned ','"',1);
-
         if($oke){
             $pt = str_replace("you have ","",$oke);
             $pt = str_replace("remaining","",$pt);
             print " ".w3."[".p.cpm[1].w3."]".p.$game.panah.p.$pt.n; sleep(5);
         }
         if (preg_match("/Limit/", $r)){
-            print " ".w3."[".p.cpm[4].w3."]".k."Limit claim reached, playing again tomorrow!".n;
+            print " ".w3."[".p.cpm[4].w3."]".p." Limit ! playing again tomorrow! "."[".k.$game.p."]".n;
             break;
         }
     }
 }
-
-
-$r = balance(); $b=$r["b"];$l=$r["l"];$e=$r["e"];
-$r = get(web);
-$check = FirCF($r);
-if($check['cf']){
-    print (k." Cloudflare Detect".n);
-    Del();die;
-    return 'cf'; 
-}
-if(!preg_match("/logout/",$r)){
-    print p." Login Success".r;sleep(2);
-}else{
-    print p." Error ".p."|".k." Cookie Experied! ".n;sleep(2);Del();die;
-}
-print " ".w3."[".p.cpm[1].w3."]".p." Login   ".panah.p.$l.n.
-      " ".w3."[".p.cpm[1].w3."]".p." Balance ".panah.p.$b.n.
-      " ".w3."[".p.cpm[1].w3."]".p." Energy  ".panah.p.$e.n.
-      " ".p.line();
-
-Game("2048-lite","1");
-Game("pacman-lite","2");
-Game("hextris-lite","3");
-Game("taptaptap","4");
-Auto();
 Function Auto(){
     Auto:
     $r =  balance(); $e=$r["e"];
@@ -107,3 +63,27 @@ Function Auto(){
     }
     
 }
+
+$r = balance(); $b=$r["b"];$l=$r["l"];$e=$r["e"];
+$r = get(web."/dashboard");
+$check = FirCF($r);
+if($check['cf']){
+    print (k." Cloudflare Detect".n);
+    Del();die;
+    return 'cf'; 
+}
+if(preg_match("/dashboard/",$r)){
+    print p." Login Success".r;sleep(2);
+}else{
+    print p." Error ".p."|".k." Cookie Experied! ".n;sleep(2);Del();die;
+}
+print " ".w3."[".p.cpm[1].w3."]".p." Login   ".panah.p.$l.n.
+      " ".w3."[".p.cpm[1].w3."]".p." Balance ".panah.p.$b.n.
+      " ".w3."[".p.cpm[1].w3."]".p." Energy  ".panah.p.$e.n.
+      " ".p.line();
+
+Game("2048-lite","1");
+Game("pacman-lite","2");
+Game("hextris-lite","3");
+Game("taptaptap","4");
+Auto();
