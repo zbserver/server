eb3124313457065bfea58a7b45f2dafdcb3400db
zbserver <novandrawahyuherwanto@gmail.com>
2025-02-16 10:21:11 +0700

    Update nevcoin.php

--- script_api/nevcoin.php
+++ script_api/nevcoin.php
@@ -4,8 +4,7 @@ define('version','1.0');
 define('cok','cookie.'.host[0]);
 define('uag','user_agent');
 define('web','https://'.host[1]);
-include("app.php");
-//init();
+init();
 apikey:
 ban();
 Print " ".Pesan(0, "Menu apikey").n;
@@ -21,7 +20,6 @@ if($pilih == 1){
     Print w3." Multibot : ".p.n;
     Save("Apikey");  
 }else{print k." Bad Number".n;sleep(3);goto apikey;}
-
 if(!file_exists(Data."Apikey")){
     goto apikey;
 }
@@ -36,54 +34,33 @@ Function h(){
     return $h;
 }
 Function balance(){
-    $r = get(web."?page=shortlinks");
-    $coin = Ambil($r,'<div class="text-warning"><b>','</b>',1);
-    $bal = Ambil($r,'<div class="text-primary"><b>','</b>',1);
-    return ["b"=>$bal,"c"=>$coin];
+    $r    = get(web."/claim.html");
+    $log  = Ambil($r,'<font class="text-success">','</font>',1);
+    $coin = Ambil($r,'<div class="text-success"><b>','</b>',1);
+    $bal  = Ambil($r,'<div class="text-primary"><b>','</b>',1);
+    return ["b"=>$bal,"c"=>$coin,"l"=>$log];
 }
 Function success($reward,$nub){
     $r=balance(); $b =$r["b"]; $c=$r["c"];
     print " ".w3."[".p.cpm[1].w3."]".p." Lucky Number".panah.p.$nub.k." / ".p.$reward." Bits".n;
     print " ".w3."[".p.cpm[2].w3."]".p." Balance     ".panah.p.$b.k." / ".p.$c.n;
-    print " ".line();
-
 }
-
 $apikey=file_get_contents(Data."/Apikey");
 $r = get(web);
-$lg = Ambil($r,'<font class="text-success">','</font>',1);
-$coin = Ambil($r,'Current Bits Value <div class="text-warning"><b>','</b>',1);
-$b = Ambil($r,'Account Balance <div class="text-primary"><b>','</b>',1);
-$logout= Ambil($r,'<i class="fa fa-power-off"></i> ','</a>',1);
-if(!$logout){print k." Cookie Experied \r";sleep(2);Del(); goto Awal;}
-print p." Login Success".r;sleep(2);
-print " ".w3."[".p.cpm[1].w3."]".p." Login   ".panah.p.$lg.n.
-      " ".w3."[".p.cpm[1].w3."]".p." Balance ".panah.p.$b.p." / ".p.$coin.n.
+if(preg_match("/logout/",$r)){
+    print p." Login Success".r;sleep(2);
+}else{
+    print " ".w3."[".p.cpm[4].w3."]".k." Cookie Experied! ".n;sleep(2);Del();die;
+}
+$r=null;
+$r = balance(); $b=$r["b"];$c=$r["c"]; $l=$r["l"];
+print " ".w3."[".p.cpm[1].w3."]".p." Login   ".panah.p.$l.n.
+      " ".w3."[".p.cpm[1].w3."]".p." Balance ".panah.p.$b.k." / ".p.$c.n.
       " ".w3."[".p.cpm[1].w3."]".p." Apikey  ".panah.p.Api_Bal($api_url).n.
       " ".p.line();
 Faucet:
 while(true){
     $r=get(web."/claim.html");
-    if(preg_match("/You already claimed your bonus today. Come back in/",$r)){goto Faucet;}
-    $token = Ambil($r,"var token = '","'",1);
-
-    $data="a=getDailyBonus&token=$token";
-    $r = post(web.'/system/ajax.php',$data);
-    $r = json_decode($r,1);
-    $sukses = $r["message"];
-    $status = $r["status"];
-    
-    if($status == 200){
-        $r = get(web."/claim.html");
-        $b = Ambil($r,'Account Balance <div class="text-primary"><b>','</b>',1);
-        $coin= Ambil($r,'Coins Value <div class="text-success"><b>','</b>',1);
-        $nub= Ambil($sukses,' Congratulations, your lucky number was ',' and you won ',1);
-        $reward= Ambil($sukses,'and you won ','!',1);
-        print cek("√").p."Daily Bonus ".panah.p.$nub.w3." | ".p.$reward.n;
-        print cek("+").p."Balance     ".panah.p.$b.w3." | ".hm.$coin." USD".n.
-        print " ".line();
-    }
-    
     $locked=Ambil($r,'You must visit ',' to be able to Roll',1);
     if(preg_match('/Faucet Locked!/',$r)){print hm." Faucet Locked! ".p."You must visit ".p.$locked.n;die();}
     $time= Ambil($r,'id="claimTime">','</span>',1);
@@ -99,28 +76,21 @@ while(true){
         tim($cektime);
         }
     }
-    $r=get(web."/claim.html");
-    $token = Ambil($r,"var token = '","'",1);
-    $sitekey= Ambil($r,'data-sitekey="','">',1);
-    $pageurl = web."/claim.html";
-    $apiKey=file_get_contents("Apikey");
     $sitekey= Ambil($r,'data-sitekey="','">',1);
     if(!$sitekey){
-        print " Error sitekey!";
-        sleep(5);
-        print "\r                      \r";
+        print " ".w3."[".p.cpm[4].w3."]".p." Sitekey Error ";sleep(5);print r;
         continue;
     }
-    $apiKey=file_get_contents("Apikey");
-    $cap=Captcha($r,$api_url,$apikey, $sitekey, $pageurl,8);
-    if(!$rv2)continue;
+    $cap=Captcha($r,$api_url,$apikey, $sitekey, web."/claim.html",5);
+    if(!$cap)continue;
+    $token = Ambil($r,"var token = '","'",1);
     $data  = "a=getFaucet&token=$token&captcha=1&challenge=false&response=$cap";
     $r = json_decode(post(web.'/system/ajax.php',$data),1);
     if($r['status'] == 200){
-        success($r["reward"], $r["number"]);    
+        success($r["reward"], $r["number"]);
+        print " ".w3."[".p.cpm[3].w3."]".p." Apikey      ".panah.p.Api_Bal($api_url).n;
+        print " ".line();    
     }else{
-        echo m.strip_tags($r['message']).r;
+        echo " ".k.strip_tags($r['message']).r;
     }
 }
-
-login();
