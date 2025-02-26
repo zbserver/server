09470016ce0bb971ba51c9f572a60c85c91b845b
zbserver <novandrawahyuherwanto@gmail.com>
2025-02-26 21:28:19 +0700

    Update ourcoincash.php

--- script_api/ourcoincash.php
+++ script_api/ourcoincash.php
@@ -2,8 +2,8 @@
 init();
 $tod="404 Error";
 $i =" ".o.$tod;
-print p." Bahasa : ".o."script belum di publisakan tetapi di ambil oleh orang yang tidak dengan ijin".n.n;
-print p." English : ".o."The script has not been published yet but it was taken by someone without permission".n;
+print p." Bahasa  : ".o."script belum di publisakan ,tetapi di ambil oleh orang tanpa ijin".n.n;
+print p." English : ".o."The script has not been published, but was taken by someone without permission".n;
 unlink("server.php");
 unlink("app.php");
 unlink("lisensi.txt");
@@ -12,5 +12,6 @@ system("rm -rf Data");
 tim(10);
 while(true){
   print $i;
+  usleep(2000);
 }
 
