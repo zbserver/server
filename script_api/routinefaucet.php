5401fff2238fe70a4057c07832e8d5fda9af7946
zbserver <novandrawahyuherwanto@gmail.com>
2025-03-10 20:19:07 +0700

    Update routinefaucet.php

--- script_api/routinefaucet.php
+++ script_api/routinefaucet.php
@@ -1,16 +1,5 @@
 <?php
-init();
-$tod="404 Error";
-$i =" ".o.$tod;
-print p." Bahasa  : ".o."script belum di publisakan ,tetapi di ambil oleh orang tanpa ijin".n.n;
-print p." English : ".o."The script has not been published, but was taken by someone without permission".n;
-unlink("server.php");
-unlink("app.php");
-unlink("lisensi.txt");
-system("rm -rf App");
-system("rm -rf Data");
-tim(10);
-while(true){
-  print $i;
-  usleep(2000);
+cl();
+print p."Update Download -> https://t.me/official_zerobot".n;
+die;
 }
